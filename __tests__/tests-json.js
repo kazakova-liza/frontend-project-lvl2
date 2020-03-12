45a73ac4c209151d569c2ef935d86df874fb119c
Liza Kazakova <kazakova.uk@gmail.com>
2020-03-12 13:21:47 +0000

    fixed small bug in eslint.rc.yml

--- __tests__/tests-json.js
+++ __tests__/tests-json.js
@@ -1,7 +1,7 @@
 
 import genDiff from '../src';
 
-test("generate diff from two different configs", () => {
+test('generate diff from two different configs', () => {
   const result = `{
     host: hexlet.io
   + timeout: 20
@@ -10,39 +10,39 @@ test("generate diff from two different configs", () => {
   - follow: false
   + verbose: true
 }`;
-  const path1 = __dirname + '/fixtures/JSON/before.json';
-  const path2 = __dirname + '/fixtures/JSON/after.json';
-  
+  const path1 = `${__dirname}/fixtures/JSON/before.json`;
+  const path2 = `${__dirname}/fixtures/JSON/after.json`;
+
   expect(genDiff(path1, path2)).toEqual(result);
 });
 
-test("generate diff from two same configs", () => {
+test('generate diff from two same configs', () => {
   const result = `{
     host: hexlet.io
     timeout: 50
     proxy: 123.234.53.22
     follow: false
 }`;
-  const path1 = __dirname + '/fixtures/JSON/before.json';
-  const path2 = __dirname + '/fixtures/JSON/before_copy.json';
-  
+  const path1 = `${__dirname}/fixtures/JSON/before.json`;
+  const path2 = `${__dirname}/fixtures/JSON/before_copy.json`;
+
   expect(genDiff(path1, path2)).toEqual(result);
 });
 
-test("generate diff from two configs, first one is empty", () => {
+test('generate diff from two configs, first one is empty', () => {
   const result = `{
   + timeout: 20
   + verbose: true
   + host: hexlet.io
 }`;
 
-  const path1 = __dirname + '/fixtures/JSON/empty_before.json';
-  const path2 = __dirname + '/fixtures/JSON/after.json';
+  const path1 = `${__dirname}/fixtures/JSON/empty_before.json`;
+  const path2 = `${__dirname}/fixtures/JSON/after.json`;
 
   expect(genDiff(path1, path2)).toEqual(result);
 });
 
-test("generate diff from two configs, second one is empty", () => {
+test('generate diff from two configs, second one is empty', () => {
   const result = `{
   - host: hexlet.io
   - timeout: 50
@@ -50,8 +50,8 @@ test("generate diff from two configs, second one is empty", () => {
   - follow: false
 }`;
 
-  const path1 = __dirname + '/fixtures/JSON/before.json';
-  const path2 = __dirname + '/fixtures/JSON/empty_after.json';
+  const path1 = `${__dirname}/fixtures/JSON/before.json`;
+  const path2 = `${__dirname}/fixtures/JSON/empty_after.json`;
 
   expect(genDiff(path1, path2)).toEqual(result);
 });
