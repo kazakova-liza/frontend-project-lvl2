2f2301c7f8e02eaeb4d012ad894680ead025b556
Liza Kazakova <kazakova.uk@gmail.com>
2020-08-14 16:01:04 +0100

    Fixed printing tree

--- src/formatters/tree.js
+++ src/formatters/tree.js
@@ -16,24 +16,28 @@ const getTreeValue = (value, depth) => {
 const makeTree = (diff) => {
   const iter = (innerDiff, depth) => {
     const space = `${'    '.repeat(depth)}`;
-    const tree = innerDiff.map((element) => {
+    const tree = innerDiff.reduce((acc, element) => {
       switch (element.status) {
         case 'same':
-          return `${space}    ${element.name}: ${getTreeValue(element.valueBefore, depth + 1)}`;
+          return [...acc, `${space}    ${element.name}: ${getTreeValue(element.valueBefore, depth + 1)}`];
         case 'changed':
-          return `${space}  - ${element.name}: ${getTreeValue(element.valueBefore, depth)}${space}  + ${element.name}: ${getTreeValue(element.valueAfter, depth)}`;
+          return [
+            ...acc,
+            `${space}  - ${element.name}: ${getTreeValue(element.valueBefore, depth)}`,
+            `${space}  + ${element.name}: ${getTreeValue(element.valueAfter, depth)}`
+          ];
         case 'deleted':
-          return `${space}  - ${element.name}: ${getTreeValue(element.valueBefore, depth)}`;
+          return [...acc, `${space}  - ${element.name}: ${getTreeValue(element.valueBefore, depth)}`];
         case 'added':
-          return `${space}  + ${element.name}: ${getTreeValue(element.valueAfter, depth)}`;
+          return [...acc, `${space}  + ${element.name}: ${getTreeValue(element.valueAfter, depth)}`];
         case undefined:
-          return `${space}    ${element.name}: ${iter(element.children, depth + 1)}`;
+          return [...acc, `${space}    ${element.name}: ${iter(element.children, depth + 1)}`];
         default:
           throw Error('Unexpected status:', element.status);
       }
-    });
+    }, []);
 
-    return `{${tree.join('\n')}${space}}`;
+    return `{\n${tree.join('\n')}\n${space}}`;
   };
   return iter(diff, 0);
 };
